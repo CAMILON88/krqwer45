93d7addd1d73dec5d3c837a5f7e870e8829eefe3
Juan Gomez Aranzazu <j.gomez267@gmail.com>
2017-07-26 10:07:45 -0500

    VERSION FINALIZADA

--- src/App.js
+++ src/App.js
@@ -1,29 +1,98 @@
 import React, { Component } from 'react';
 
-/// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
-
 class App extends Component {
   constructor() {
     super()
+    
+
     this.state = {
       tasks: [
         { id: 1, name: "Sacar la ropa", done: false },
         { id: 2, name: "Hacer la cama", done: true },
         { id: 3, name: "Leer un rato", done: false }
       ],
-      newTask: ''
+      newTask: '',
+      count: 4,
+
+      
+    
     }
   }
+  
+
+ 
+  textChange(event) {
+    this.setState({
+      newTask: event.target.value
+    })
+  }
+
+ 
+  addItem(event)
+  {
+     
+   
+     event.preventDefault();
+     if(this.state.newTask !== '')
+     {
+
+
+     this.setState({
+      tasks: this.state.tasks.concat({id: this.state.count++, name:this.state.newTask, done: false}),
+      newTask: ''
+
+     
+    })
+   }
+
+
+
+
+   }
+
+
+chequear(index) 
+{
+
+this.setState({
+
+  tasks: [ 
+
+  { 
+    id: this.state.tasks[index].id,
+    name: this.state.tasks[index].name,
+    done: !this.state.tasks[index].done 
+
+  }
+    
+  ]
+
+  
+})
+
+
+
+
+
+
+ 
+
+}
+ 
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+            {this.state.tasks.map((task, index) => <li  key={task.id} className={task.done ? "done" : ""} onClick={this.chequear.bind(this, index)}>{task.name}</li>)}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+
+          <form onSubmit={this.addItem.bind(this)}>
+           
+            <input className="error" type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter"  value={this.state.newTask} onChange={this.textChange.bind(this)} required/>
+          
           </form>
         </div>
       </div>
